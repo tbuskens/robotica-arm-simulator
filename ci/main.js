5dd54cb0f1e6f25aa86583c0a6b7f52dc648440f
tbuskens <t.buskens@twizzel.net>
2019-03-07 12:36:28 +0100

    IDK how this works...

--- ci/main.js
+++ ci/main.js
@@ -1,7 +1,7 @@
 (() => {
   'use strict';
   const assert = require('assert');
-  const myHello = require('../public_html/index.js');
+  const myHello = require('./public_html/index.js');
   describe('Test', function() {
     it('should say hello before something', function() {
       assert.equal(myHello.hello('test'), 'hello test');
